1d78bd576444e3a2b261a09656795b50c5ed9849
EduFatou <edufatou@gmail.com>
2024-06-24 09:59:23 +0200

    fixed .env

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -12,26 +12,29 @@ require('dotenv').config()
 // });
 
 //RENDER
-const pool = new Pool({
-    host: 'dpg-cprhqvdumphs73c615n0-a.frankfurt-postgres.render.com',
-    user: 'edu_hc2f_user',
-    port: '5432',
-    database: 'edu_hc2f',
-    password: 'U1gvLe9UnuO1ll1wnZvnMJZMQCIEFSFe',
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+// const pool = new Pool({
+//     host: 'dpg-cprhqvdumphs73c615n0-a.frankfurt-postgres.render.com',
+//     user: 'edu_hc2f_user',
+//     port: '5432',
+//     database: 'edu_hc2f',
+//     password: 'U1gvLe9UnuO1ll1wnZvnMJZMQCIEFSFe',
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
 
 
 
 //DOTENV
-//   const pool = new Pool({ 
-//     user: process.env.PG_USER, 
-//     host: process.env.PG_HOST, 
-//     database: process.env.PG_DATABASE, 
-//     password: process.env.PG_PASSWORD,
-//     port: process.env.PG_PORT
-// })
+  const pool = new Pool({ 
+    user: process.env.DB_USER, 
+    host: process.env.DB_HOST, 
+    database: process.env.DB_DATABASE, 
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+    rejectUnauthorized: false
+    }
+})
 
 module.exports = pool;
